aae70cf2a099c2cdd60aefc47dd16ef8c0441354
Hunter Koppen <hkoppen@clevr.com>
2022-07-05 01:17:19 +0200

    Added normal html5 element

--- src/components/H5AudioPlayer.jsx
+++ src/components/H5AudioPlayer.jsx
@@ -1,27 +1,21 @@
 import { Component, createElement } from "react";
 
-import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
-
 export class H5AudioPlayer extends Component {
     state = {
         filepath: null
     };
 
-    componentDidMount() {
-        // If the url is set we use that as a audio source
-        if (this.props.url) {
-            this.setState({
-                filepath: this.props.url
-            });
-        }
-    }
-
     componentDidUpdate() {
-        // Check if widget has loaded the file data
-        const file = this.props.file;
-        if (file && file.status === "available") {
+        const { file, url } = this.props;
+        if (url && url.status === "available") {
+            if (this.state.filepath !== url.value) {
+                this.setState({
+                    filepath: url.value
+                });
+            }
+        } else if (file && file.status === "available") {
             if (this.state.filepath !== file.value.uri) {
+                console.log(file.value.uri);
                 this.setState({
                     filepath: file.value.uri
                 });
@@ -30,12 +24,6 @@ export class H5AudioPlayer extends Component {
     }
 
     render() {
-        return (
-            <AudioPlayer
-                autoPlay
-                src={this.state.filepath}
-                // other props here
-            />
-        );
+        return <audio autoplay controls src={this.state.filepath}></audio>;
     }
 }
